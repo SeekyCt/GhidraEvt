27bb558157ddf8ae5d19e3b9ebb7c74e142f3206
Seeky <58006653+SeekyCt@users.noreply.github.com>
2026-08-28 14:18:51 +0100

    Add license notices

--- src/main/java/ghidraevt/location/EvtLocationMemento.java
+++ src/main/java/ghidraevt/location/EvtLocationMemento.java
@@ -1,3 +1,22 @@
+/* ###
+ * IP: GHIDRA
+ *
+ * Copyright 2026 SeekyCt
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Modified from Ghidra's decompiler UI source code to work on evt scripts
+ */
 package ghidraevt.location;
 
 import ghidra.app.nav.LocationMemento;
